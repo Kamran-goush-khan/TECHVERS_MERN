568fa11bfc21567fe575378e3584df34eb6f7da1
Kamran-goush-khan <100296501+Kamran-goush-khan@users.noreply.github.com>
2024-02-18 22:15:39 +0530

    Add Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,20 @@ import About from './pages/About';
 import Project from './pages/Project';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-
-
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/dashboard" element={<DashBoard />}></Route>
-          <Route path="/project" element={<Project />}></Route>
+          <Route path="/projects" element={<Project />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
-          <Route path="/sign-up" element={<SignUp/>}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
         </Routes>
       </BrowserRouter>
     </>
