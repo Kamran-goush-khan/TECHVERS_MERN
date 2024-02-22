63dedb01f03417915d071771e496e6bd83ba0ac2
Kamran-goush-khan <100296501+Kamran-goush-khan@users.noreply.github.com>
2024-02-22 13:04:06 +0530

    create a middleware And a function to handle error

--- api/index.js
+++ api/index.js
@@ -22,4 +22,14 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', UserRoutes);
-app.use('/api/auth', AuthRoutes)
+app.use('/api/auth', AuthRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
