348938d6a262f170183ea70c0f2d84c7b0146129
Kamran-goush-khan <100296501+Kamran-goush-khan@users.noreply.github.com>
2024-02-22 15:33:20 +0530

    Add functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Label, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password || !formData.email) {
+      return setErrorMessage('All Fields are required');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+ 
+    } catch (err) {
+      setLoading(false);
+      setErrorMessage(err.message);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -16,53 +56,69 @@ const SignUp = () => {
             Verse
           </Link>
           <p className="text-sm mt-5">
-            This is the home for the tech enthusiast people where you can learn
-            and find different blog related to modern technology.
+            A professional blogging platform for tech enthusiasts. Create,
+            share, and discuss tech content with a dedicated community.
           </p>
         </div>
 
         {/* right side  */}
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={submitHandler}>
             <div>
               <Label value="Your UserName" />
               <TextInput
                 type="text"
                 placeholder="name"
                 id="username"
+                onChange={changeHandler}
               />
             </div>
 
             <div>
               <Label value="Your Email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="name@gmail.com"
                 id="email"
+                onChange={changeHandler}
               />
             </div>
 
             <div>
               <Label value="Your Password" />
               <TextInput
-                type="text"
+                type="password"
                 placeholder="********"
                 id="password"
+                onChange={changeHandler}
               />
             </div>
 
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>...Loading</span>
+                  </>
+                ) : 'Sign up'
+              }
             </Button>
-
-            <div className="flex gap-2 mt-5">
-              <span>Already have an account?</span>
-              <Link to='/sing-in' className='text-blue-500'>
-                Sing In
-              </Link>
-            </div>
           </form>
+
+          <div className="flex gap-2 mt-5">
+            <span>Already have an account?</span>
+            <Link to="/sing-in" className="text-blue-500">
+              Sing In
+            </Link>
+          </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
