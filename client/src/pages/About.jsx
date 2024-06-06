fa034fbc17717e29e6d9521cf661e7c14f9b04cd
Kamran-goush-khan <100296501+Kamran-goush-khan@users.noreply.github.com>
2024-06-07 00:58:45 +0800

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
+export default function About() {
   return (
-    <div>
-      about
-    </div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About TechVerse
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              TechVerse Blog is a dynamic platform where tech enthusiasts
+              worldwide come together. With a wide range of content, from
+              programming tutorials to cybersecurity insights, there's something
+              for everyone. Join our community to connect, learn, and stay
+              updated on the latest in tech.
+            </p>
 
-export default About
+            <p>
+              Explore our curated articles and tutorials covering various tech topics. Engage in discussions with like-minded individuals, share your expertise, and learn from others' experiences. With TechVerse Blog, you're part of a vibrant community dedicated to fostering knowledge sharing and collaboration.
+            </p>
+
+            <p>
+              Join us today and elevate your tech journey with valuable resources, interactive discussions, and connections with fellow tech enthusiasts. Whether you're a beginner or an expert, TechVerse Blog is your go-to destination for all things tech-related.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
